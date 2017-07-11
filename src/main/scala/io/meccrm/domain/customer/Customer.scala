f101922e5b830fcc4e99212543d33062da98f17e
Angelos M <angelos.michos@gmail.com>
2017-07-11 23:21:40 +0100

    Adds Customer model

---
+++ src/main/scala/io/meccrm/domain/customer/Customer.scala
@@ -0,0 +1,12 @@
+package io.meccrm.domain.customer
+
+
+case class Customer(name: String, address: Address, trn: TrnRef, pfs: PfsRef, phoneNum: String)
+
+case class Address(address: String, city: String, country: String, postCode: String)
+
+//NOTE: Tax Registration Number
+case class TrnRef(trn: String)
+
+//NOTE: Public Financial Service
+case class PfsRef(pfs: String)
